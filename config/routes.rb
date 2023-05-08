cb98488becf691d3185f550fc656ba48c8cd52e1
Shikha Panchal <gshikha@anviam.com>
2023-05-08 17:40:00 +0530

    crud of message example

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "messages#index"
+  resources :messages do
+    member do
+      post :edit
+    end
+  end
 end
